495a34b46689a4130dd0a7b8a9904bcb9dde9403
MigeulEliotte <97182631+MigeulEliotte@users.noreply.github.com>
2024-11-26 23:16:10 -0600

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+);
+
+CREATE TABLE quizzes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    subject TEXT NOT NULL,
+    questions_count INTEGER NOT NULL,
+    quiz_date DATE NOT NULL
+);
+
+CREATE TABLE quiz_results (
+    student_id INTEGER,
+    quiz_id INTEGER,
+    score INTEGER NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id),
+    PRIMARY KEY (student_id, quiz_id)
+);
